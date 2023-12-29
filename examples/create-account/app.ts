e37121f203dab1dcbdd2468d897d888203563eae
Derek Chiang <me@derekchiang.com>
2023-12-28 22:51:40 -0500

    Move examples to root

--- examples/create-account/app.ts
+++ examples/create-account/app.ts
@@ -1,23 +1,23 @@
-import "dotenv/config"
+import { createKernelAccount } from "@kerneljs/core"
 import { signerToEcdsaValidator } from "@kerneljs/ecdsa-validator"
+import "dotenv/config"
 import { http, createPublicClient } from "viem"
 import { privateKeyToAccount } from "viem/accounts"
 import { polygonMumbai } from "viem/chains"
-import { createKernelAccount } from "@kerneljs/core"
 
 const publicClient = createPublicClient({
-  transport: http(
-    `https://meta-aa-provider.onrender.com/api/v2/bundler/${process.env.ZERODEV_PROJECT_ID}`
-  ),
-  chain: polygonMumbai
+    transport: http(
+        `https://meta-aa-provider.onrender.com/api/v2/bundler/${process.env.ZERODEV_PROJECT_ID}`
+    ),
+    chain: polygonMumbai
 })
 
 const signer = privateKeyToAccount(process.env.PRIVATE_KEY as "0x...")
 
 const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
-  signer,
+    signer
 })
 
 const account = createKernelAccount(publicClient, {
-  plugin: ecdsaValidator,
+    plugin: ecdsaValidator
 })
